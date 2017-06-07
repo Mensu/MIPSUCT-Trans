cb28acd7581476da79aa97d8f54df89b3a2b1cca
pfjhyyj <pfjhyyj@qq.com>
2017-06-08 06:13:50 +0800

    final

--- BallTree/src/test.cpp
+++ BallTree/src/test.cpp
@@ -172,9 +172,5 @@ void TestDataSets() {
 } // anonymous namespace
 
 int main() {
-<<<<<<< HEAD
-    TestDataSets<Yahoo>();
-=======
     TestDataSets<Mnist>();
->>>>>>> 21c1a5c70991cc9afb6531d56bfb2d723ff25d19
 }
