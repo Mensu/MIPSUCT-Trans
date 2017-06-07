d02fd1a3ba9cecc3c1a7ff2d739eaa71dbfb4dae
pfjhyyj <pfjhyyj@qq.com>
2017-06-08 06:02:43 +0800

    minor fix

--- BallTree/src/NodeBuilder.cpp
+++ BallTree/src/NodeBuilder.cpp
@@ -7,20 +7,12 @@ void NodeStorer::Visit(BallTreeBranch* branch) {
 	branch->r_right = branch->right->rid;
 	Rid r = node_storage_->Put(*branch);
 	branch->rid = r;
-	// auto before_cast = node_storage_->Get(r);
-	// auto ptr = dynamic_cast<BallTreeBranch *>(before_cast.get());
-	// assert(ptr != nullptr);
-	// assert(*ptr == *branch);
 }
 
 void NodeStorer::Visit(BallTreeLeaf* leaf) {
 	leaf->data = StoreAll(leaf->raw_data);
 	Rid r = node_storage_->Put(*leaf);
 	leaf->rid = r;
-	// auto before_cast = node_storage_->Get(r);
-	// auto ptr = dynamic_cast<BallTreeLeaf *>(before_cast.get());
-	// assert(ptr != nullptr);
-	// assert(*ptr == *leaf);
 }
 
 std::vector<Rid> NodeStorer::StoreAll(const Records& records) {
